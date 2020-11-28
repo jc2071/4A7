cdac791109514216e3e56fe31a931c476d9c0d59
Jonathan Collins <jc2071@cam.ac.uk>
2020-11-28 14:50:30 +0000

    ISA function

---
+++ ISA.m
@@ -0,0 +1,30 @@
+function [T,p,rho,a] = ISA(h)
+% [T,p,rho,a] = ISAt(h)
+% T = tempreture
+% p = pressure
+% rho = density
+% a = speed of sound
+% h = height in km above sl
+
+tsl = 288.15;
+asl = 340.3;
+psl = 101.325;
+rhosl = 1.225;
+
+if h <= repmat(11,size(h)) && repmat(11,size(h)) >=0
+    T = tsl - 6.5.*h;
+    p = psl*(T./tsl).^(5.256);
+    rho = rhosl*(T./tsl).^(4.256);
+elseif 11 < h && h <= 20
+    T = 216.65;
+    rhot = rhosl*(T./tsl).^(4.256);
+    pt = psl*(T./tsl).^(5.256);
+    p = pt*exp(-0.1577*(h -11));
+    rho = rhot*exp(-0.1577*(h-11));
+else
+    error('Please enter alitude 0 <= h <= 20')
+end
+
+a = asl*(T./tsl).^0.5;
+
+end
---
+++ ref.m
@@ -0,0 +1,41 @@
+%% ISA base values
+
+tsl = 288.15;
+asl = 340.3;
+psl = 101.325;
+rhosl = 1.225;
+
+%Reference data 1
+
+pmax = 240; % max number of passengers
+rmax = 12000; % range at max payload (km)
+plmax = 40; % max payload (tonnes)
+we = 106; % empty weight (tonnes)
+fmax = 74; % max fuel capacity at max payload (tonnes)
+tmax = 220; % max take off weight (tonnes)
+cr = 256; % cruie TAS (Target Air Speed, V)
+mc = 0.8; % mach number at cruise
+icr = 31000; % initial cruise altitude (ft)
+LD = 21; % cruise L/D
+A = 315; % wing area
+
+%% Reference data 2
+tac = 226;
+pac = 28.7;
+to2 = 259;
+po2 = 45.7;
+OPR = 45;
+theta = 6;
+teff = 0.9;
+FPR = 1.45;
+feff = 0.92;
+traneff = 0.90;
+
+
+%% Refrence data 3
+
+k = 0.015;
+c1 = 0.3;
+c2 = 1.0;
+k1 = 0.0125;
+k2 = 0.0446;
